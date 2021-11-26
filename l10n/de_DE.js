a362edce6b46cd097895f6abbb15b360eb8852a4
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2021-11-26 09:58:57 +0530

    Translation added

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -2,6 +2,8 @@ OC.L10N.register(
     "nmc_email_templates",
     {
     "Open share" : "Offen teilen",
-	"Life is for sharing" : "Das Leben ist zum Teilen da"
+	"Life is for sharing" : "Das Leben ist zum Teilen da",
+	"has shared" : "hat geteilt",
+	"With You. Click button below to accept and open it." : "Mit dir. Klicken Sie auf die Schaltfläche unten, um sie zu akzeptieren und zu öffnen."
 },
 "nplurals=2; plural=(n != 1);");
