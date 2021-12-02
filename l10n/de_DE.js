07d72cde307707e5b41495b4c0080b8741e51cea
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2021-12-02 08:13:46 +0530

    Translations file updated for welcome email

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -6,6 +6,17 @@ OC.L10N.register(
 	"has shared" : "hat geteilt",
 	"Hello"	: "Hallo",
 	"There were the following activities in your": "Folgende Aktivitäten gab es in Ihrer",
-	"With You. Click button below to accept and open it." : "Mit dir. Klicken Sie auf die Schaltfläche unten, um sie zu akzeptieren und zu öffnen."
+	"With You. Click button below to accept and open it." : "Mit dir. Klicken Sie auf die Schaltfläche unten, um sie zu akzeptieren und zu öffnen.",
+
+	"All data securely in one place" : "Alle Daten sicher an einem Ort",
+	"we have activated your MagentaCLOUD, Telekom's online storage. Just click the link, log in and you are ready to go." : "wir haben ihre MagentaCLOUD, den Online-Speicher der Telekom, freigeschaltet. Einfach den Link anklicken, einloggen und Sie sind startklar.",
+	"With MagentaCLOUD, you can store your photos, videos, documents and much more securely online and access them from anywhere using your personal access data." : "Mit der MagentaCLOUD können Sie Ihre Fotos, Videos, Dokumente und vieles mehr sicher online speichern und mit Ihren persönlichen Zugangsdaten von überall abrufen.",
+	"Keep memories safe" : "Erinnerungen sicher aufbewahren",
+	"Share special moments with family" : "Besondere Momente mit der Familie teilen",
+	"Keep all files in overview" : "Alle Dateien im Überblick behalten",
+	"Expand device memory free of charge" : "Gerätespeicher kostenlos erweitern",
+	"You can find more information and frequently asked questions about MagentaCLOUD at" : "www.telekom.de/magentacloud",
+	"Your Telekom" : "Ihre Telekom"
+
 },
 "nplurals=2; plural=(n != 1);");
