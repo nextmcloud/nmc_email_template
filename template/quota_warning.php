e1179f615033bb83f725e54aa710884f9e1bb08f
TSI-ashishsharma <sharma.a@t-systems.com>
2021-12-03 13:28:39 +0530

    dynamic variables

--- template/quota_warning.php
+++ template/quota_warning.php
@@ -10,15 +10,15 @@
 	   <tr style="background: #f1f1f1;">
 		 <td style="font-family: sans-serif; font-size: 12px; vertical-align: top;padding-left: 32px;padding-right: 16px">
 		   <div style="text-align: center;width: 100% ;padding-top: 48px;box-sizing: border-box;">
-			 <span style="font-size: 32px;color:#e20074">64,7</span><span style="font-size: 16px;"> GB</span>
+			 <span style="font-size: 32px;color:#e20074">'.$this->data['usedSpace'][0].'</span><span style="font-size: 16px;"> '.$this->data['usedSpace'][1].'</span>
 			 <br />
-			 <div style="width:110px;display: inline-block;margin-bottom: 32px;border-top: 1px solid #191919;"><span style="font-size: 32px;color:#191919;">500</span><span style="font-size: 16px;"> GB</span></div>
+			 <div style="width:110px;display: inline-block;margin-bottom: 32px;border-top: 1px solid #191919;"><span style="font-size: 32px;color:#191919;">'.$this->data['quota1'][0].'</span><span style="font-size: 16px;"> '.$this->data['quota1'][1].'</span></div>
 		   </div>
 		   </td>
 		   <td style="font-family: sans-serif; font-size: 12px; vertical-align: top;padding-right: 24px;padding-left: 16px">
 			 <div style="text-align: left;width: 100% ;padding-top: 24px;padding-bottom: 24px;box-sizing: border-box;">
 			 <span style="font-weight: bold;">Quota Warning</span>
-			   <p style="font-size: 12px;margin-top: 8px;margin-bottom: 16px;"><span style="font-size: 12px;font-weight: bold;">85%</span> of your storage is currently occupied.Once your memory is used up,you cannot upload any more files.However,you can remove files or empty you recycle bin at any time</p>
+			   <p style="font-size: 12px;margin-top: 8px;margin-bottom: 16px;"><span style="font-size: 12px;font-weight: bold;">'.$this->data['quota'].' %</span> of your storage is currently occupied.Once your memory is used up,you cannot upload any more files.However,you can remove files or empty you recycle bin at any time</p>
 			   <a href="#" target="_blank" style="display: inline-block;color: #191919;background-color: #f1f1f1 !important;border: 1px solid #191919;border-radius: 8px;box-sizing: border-box;cursor: pointer;text-decoration: none;font-size: 12px;font-weight: bold;margin: 0;padding: 12px 24px;text-transform: capitalize;margin-right: 8px;">Expand Storage</a>
 			   <a href="#" target="_blank" style="display: inline-block;color: #191919;background-color: #f1f1f1 !important;border: 1px solid #191919;border-radius: 8px;box-sizing: border-box;cursor: pointer;text-decoration: none;font-size: 12px;font-weight: bold;margin: 0;padding: 12px 24px;text-transform: capitalize;">Open the trash</a>
 			 </div>
@@ -29,7 +29,7 @@
 	 <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="margin-top:32px;border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
 	   <tr>
 		 <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
-		   <p style="font-family: sans-serif; font-size: 14px; font-weight: bold; margin: 0; Margin-bottom: 16px;">Hello "Display Name of recipient",</p>
+		   <p style="font-family: sans-serif; font-size: 14px; font-weight: bold; margin: 0; Margin-bottom: 16px;">Hello "'.$this->data['shareWithDisplayName'].'",</p>
 		   <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 16px;">with the MagentaCLOUD status email,we inform you once a month about the storage space you have used and the shares you have created.</p>
 		   <p style="margin-top:16px;margin-bottom:32px">Your Telekom</p>
 		   <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
