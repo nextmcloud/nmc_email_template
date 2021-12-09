d621629501874408eca10769d2fac14f0a9d321b
naresh rai <naresh.rai@t-systems.com>
2021-12-09 16:03:32 +0530

    png logo added instead of svg

--- template/welcome_mail.php
+++ template/welcome_mail.php
@@ -98,7 +98,7 @@
 			<tr>
 			<td class="content-block" style="background: #e20074;font-family: sans-serif; vertical-align: top; padding-bottom: 16px; padding-top: 16px; padding-left:24px; font-size: 12px; color: #999999; text-align: left;
 ">
-			<img src="'.$this->urlPath.'themes/nextmagentacloud21/core/img/logo.png" width="72px" height="35px"/>
+			<img src="'.$this->urlPath.'themes/nextmagentacloud21/core/img/logo-new.png" width="72px" height="35px"/>
 			<span style="color: #fff; font-size: 16px; text-align: left;font-weight:bold;line-height: 40px;padding-left: 24px;vertical-align: top;">'.$this->l10n->t('Life is for sharing').'</span>
 			</td>
 			</tr>
