7436f7b33512efe253184e111f20e3e98c7b0e6c
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-01-17 10:52:33 +0530

    Header test case added

--- tests/EMailTemplateTest.php
+++ tests/EMailTemplateTest.php
@@ -46,6 +46,7 @@ class EMailTemplateTest extends TestCase {
 	protected $l;
 	protected $text;
 	protected $footer;
+	protected $header;
 
 	protected function setUp(): void {
 		parent::setUp();
@@ -139,6 +140,33 @@ class EMailTemplateTest extends TestCase {
 	  $expectedHTML = file_get_contents(\OC::$SERVERROOT . '/apps/nmc_email_template/tests/data/emails/footer.html');
 	  $rendredHTML = $this->footer;
 	  $this->assertSame(trim($expectedHTML), $rendredHTML);
+	}
+
+	public function testEMailTemplateDefaultHeader() {
+		$this->header = '
+	<div class="header" style="clear: both;text-align: center; width: 100%;border-bottom:2px solid #e5e5e5;">
+		<table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
+			<tr>
+				<td class="content-block" style="background: #e20074;font-family: sans-serif; vertical-align: top; padding-bottom: 16px; padding-top: 16px; padding-left:24px; font-size: 12px; color: #999999; text-align: left;">
+					<img src="host_namethemes/nextmagentacloud21/core/img/logo-new.png" width="72px" height="35px"/>
+					<span style="color: #fff; font-size: 16px; text-align: left;font-weight:bold;line-height: 40px;padding-left: 24px;vertical-align: top;">Life is for sharing.</span>
+				</td>
+			</tr>
+			<tr>
+				<td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; padding-left:24px; font-size: 12px; text-align: center;border-top-right-radius:8px;border-top-left-radius:8px">
+					<p style="color: #191919; font-size: 25px; text-align: left;font-weight: bold;margin:4px 0">Magenta<span style="font-size: 25px; font-weight: normal;">CLOUD</span></p>
+				</td>
+			</tr>
+		</table>
+	</div>
+		';
+		$sloganTranslated = $this->l10n->t('Life is for sharing');
+		$this->header = str_replace('host_name',$this->urlPath, str_replace('Life is for sharing', $sloganTranslated, $this->header));
+
+		$rendredHTML = include_once 'nmc_email_template/tests/data/emails/header.php';
+		$rendredHTML = rtrim($rendredHTML,"1");
+		$expectedHTML = $this->header;
+		$this->assertSame(trim($expectedHTML), $rendredHTML);
 	}
 
 }
